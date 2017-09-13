fd98a2ad5aa19dd9718acb64cfe41bc7cf9adeed
Unknown <zalozny_kirill@mail.ru>
2017-09-13 22:17:26 +0500

    testing

---
+++ what/what.cpp
@@ -0,0 +1,15 @@
+// Test.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl
+		<< "Testing..." << endl;
+	system("pause");
+	return 0;
+}
+
