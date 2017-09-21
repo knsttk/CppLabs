43c4a5e2b555e6ba1222f751487347d7927f9c13
Unknown <zalozny_kirill@mail.ru>
2017-09-21 17:25:45 +0500

    3 edited

--- Лабораторные/Лабораторная1/Задание3/Задание3.cpp
+++ Лабораторные/Лабораторная1/Задание3/Задание3.cpp
@@ -6,14 +6,13 @@ using namespace std;
 int main()
 {
 	int n;
-		cout << "Enter N: " << endl; // Просим ввести N
+		cout << "Enter N: " << endl; 
 	int sum = 0; // Объявляем целочисленную переменную, которая будет являться суммой цифр в N
-		cin >> n; // Вводим N
-	while (n > 0) // Запускаем цикл с предусловием
+		cin >> n; 
+	while (n > 0) 
 {		sum = sum + n % 10; // Заставляем цикл прибавлять цифру из N с каждой новой итерацией
 		n = n / 10; // Уменьшаем число N на разряд с каждой новой итерацией
 }
-	cout << "Summa =" << sum << endl; // Выводим сумму цифр
-	system("pause");
+	cout << "Summa =" << sum << endl; 
 	return 0;
 }
