6c1b52e43bd7aa79283affc6d1e5830b01c1d1c0
Unknown <zalozny_kirill@mail.ru>
2017-10-07 20:01:42 +0500

    Lab2_1

---
+++ Лабораторные/Лабораторная2/Задание1/Зд1.cpp
@@ -0,0 +1,45 @@
+// Lab2, Variant 9, Ex.1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+		cout << "Введите к-во элементов массива: " << endl;
+	int size;
+		cin >> size;
+	int* mas = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Введите " << i + 1 << "-ый элемент: " << endl;
+		cin >> mas[i];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (mas[i] % 2 != 0 && mas[i + 1] % 2 == 0) // Запуск проверки на следование нечётного за чётным
+		{
+			for (int i = size - 1; i >= 0; i--)
+			{
+				if (mas[i] > 0)
+				{
+					cout << "a[" << i << "]=" << mas[i] << endl; // Организация вывода положительных элементов в обратном порядке
+				}
+			}
+		}
+		else
+		{
+			for (int i = size - 1; i >= 0; i--)
+			{
+				if (mas[i] < 0)
+				{
+					cout << "a[" << i << "]=" << mas[i] << endl;
+				}
+			}
+		}
+	}
+	delete [] mas;
+
+	system("pause");
+	return 0;
+}
