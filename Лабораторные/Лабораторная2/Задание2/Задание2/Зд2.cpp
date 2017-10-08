b064385b908eff8f068c2a2d0ec9d4d877b5e620
Unknown <zalozny_kirill@mail.ru>
2017-10-08 16:51:33 +0500

    2 zad complete

--- Лабораторные/Лабораторная2/Задание2/Задание2/Зд2.cpp
+++ Лабораторные/Лабораторная2/Задание2/Задание2/Зд2.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main()
 {
-
+	setlocale(LC_ALL, "Russian");
+	cout << "¬ведите к-во элементов массива: " << endl;
+	int size;
+	cin >> size;
+	int* mas = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "¬ведите " << i + 1 << "-ый элемент: " << endl;
+		cin >> mas[i];
+	}
+	int t;
+	for (int i = 0; i < size; i++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (mas[i] < mas[i + 1] && mas[i] < 0 && mas[i + 1] > 0)
+			{
+				t = mas[i];
+				mas[i] = mas[i + 1];
+				mas[i + 1] = t;			}
+		}
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << "a[" << i << "] = " << mas[i] << endl;
+	}
+	delete[] mas;
+	system("pause");
 	return 0;
 }
